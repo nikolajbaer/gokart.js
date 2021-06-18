87def4da162fb110e5e13708ba7472a21469a4ba
Nikolaj Baer <nikolaj.baer@gmail.com>
2021-06-18 07:06:19 -0700

    wip still trying to get jest going for three jsms

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: ["js", "jsx"],
+  moduleDirectories: ["node_modules", "src"],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  }
+};
